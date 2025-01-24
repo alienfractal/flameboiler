c33e551a610d818f845919cd68f37dd8f8e9a87b
John <dreamcodesoft@gmail.com>
2025-01-24 23:10:08 +0200

    someone update this

--- flameboilerplate/android/app/src/main/kotlin/com/example/coolorburn/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.coolorburn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flameboilerplate/lib/gamefsm/istate.dart
+++
@@ -1,8 +0,0 @@
-// Abstract class to act as an interface for game states
-import 'package:coolorburn/gamefsm/fsm.dart';
-
-abstract class IState {
-  void enter(Fsm gameFsm);
-  void update(double dt);
-  void exit(Fsm gameFsm);
-}
